a980710d6039ae411d33ecd169431f7e2f97c2f2
PragatiPaliwal35 <89850247+PragatiPaliwal35@users.noreply.github.com>
2021-08-31 18:52:57 +0530

    Create reversed.c
    Reverse number program.

---
+++ reversed.c
@@ -0,0 +1,23 @@
+2.	Write a code to reverse a number.
+
+#include<stdio.h>  
+ #include<stdio.h>  
+ int main()    
+{    
+int n, reverse=0, rem;    
+printf("Enter a number: ");    
+  scanf("%d", &n);    
+  while(n!=0)    
+  {    
+     rem=n%10;    
+     reverse=reverse*10+rem;    
+     n/=10;    
+  }    
+  printf("Reversed Number: %d",reverse);    
+return 0;  
+}  
+
+Output : enter a number456
+Reversed number654
+ 
+
